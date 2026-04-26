08e2fabf5162010e7fff9c88d4c5f3a72486f299
jembeekart-design <jembeekart@gmail.com>
2026-04-27 01:00:12 +0530

    Update BannerUploader.tsx

--- src/components/admin/BannerUploader.tsx
+++ src/components/admin/BannerUploader.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useState } from "react";
-import { compressImage } from "@/lib/imageCompression";
 
 type Props = {
   onChange: (file: File, preview: string) => void;
