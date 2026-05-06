48da90e5df4b3c9bf043e71cbfda8a6db37e6aac
jembeekart-design <jembeekart@gmail.com>
2026-05-07 05:00:46 +0530

    Update HomeScreen.tsx

--- src/shared/screens/HomeScreen.tsx
+++ src/shared/screens/HomeScreen.tsx
@@ -1,28 +1,26 @@
 "use client";
 
-import Navbar from "@/components/layout/Navbar";
+import Navbar from "@/components/Navbar";
+import CategoryBar from "@/shared/components/CategoryBar";
 import Banner from "@/components/layout/Banner";
 import Section from "@/components/sections/Section";
 import NavItem from "@/components/ui/NavItem";
 import { products } from "@/data/products";
-import CategoryBar from "@/shared/components/CategoryBar";
 
 export default function HomeScreen() {
   return (
     <div className="bg-gray-100 min-h-screen pb-20">
 
       {/* Navbar */}
-<div className="bg-blue-500 p-3 sticky top-0 z-50">
-  <input
-    className="w-full p-3 rounded-xl outline-none"
-    placeholder="Search for Products"
-  />
-</div>
+      <Navbar />
 
-{/* Category Bar */}
-<CategoryBar />
+      {/* Category Icons */}
+      <CategoryBar />
+
+      {/* Banner */}
       <Banner />
 
+      {/* Sections */}
       <Section title="Best Value Deals on Fashion" products={products} />
       <Section title="Add to your wishlist" products={products} blue />
       <Section title="Home Decor & Furnishing" products={products} />
