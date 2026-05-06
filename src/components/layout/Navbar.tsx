fbd81fd3e0ad76595fcec34b0f7ab38dc86d66b9
jembeekart-design <jembeekart@gmail.com>
2026-05-06 19:32:52 +0530

    Create Navbar.tsx

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <div className="bg-blue-500 p-3 sticky top-0 z-50">
+      <input
+        className="w-full p-3 rounded-xl outline-none"
+        placeholder="Search for Products"
+      />
+    </div>
+  );
+}
