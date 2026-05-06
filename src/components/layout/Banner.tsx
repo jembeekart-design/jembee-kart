3a49b8ae576bb7901e8c14718a1567fbd7826106
jembeekart-design <jembeekart@gmail.com>
2026-05-06 20:20:09 +0530

    Add Banner component for displaying a banner slider

---
+++ src/components/layout/Banner.tsx
@@ -0,0 +1,9 @@
+export default function Banner() {
+  return (
+    <div className="p-2">
+      <div className="bg-black rounded-xl h-40 flex items-center justify-center text-white">
+        Banner Slider
+      </div>
+    </div>
+  );
+}
