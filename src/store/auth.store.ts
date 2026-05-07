07c5fd80de6078213e2f4b06081bcac9cff3142b
jembeekart-design <jembeekart@gmail.com>
2026-05-07 22:56:19 +0530

    Create auth.store.ts for user authentication state

---
+++ src/store/auth.store.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+interface AuthStore {
+  user: any;
+  setUser: (user: any) => void;
+}
+
+export const useAuthStore = create<AuthStore>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+}));
+
+// THEME ADMIN PANEL SE CHANGE HOGA
