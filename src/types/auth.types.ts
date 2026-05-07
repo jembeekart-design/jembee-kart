016c1175814c22890dcca50f34ef4d6f6b173d61
jembeekart-design <jembeekart@gmail.com>
2026-05-07 22:59:35 +0530

    Add User and LoginPayload interfaces

---
+++ src/types/auth.types.ts
@@ -0,0 +1,13 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+}
+
+export interface LoginPayload {
+  email: string;
+  password: string;
+}
+
+// THEME ADMIN PANEL SE CHANGE HOGA
