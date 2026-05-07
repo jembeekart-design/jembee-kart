f58e9c84900bfd66f285b90d33e84f27969af693
jembeekart-design <jembeekart@gmail.com>
2026-05-07 22:50:15 +0530

    Add Firestore configuration file

---
+++ src/config/firestore.config.ts
@@ -0,0 +1,8 @@
+import { getFirestore } from "firebase/firestore";
+import app from "./firebase.config";
+
+const firestore = getFirestore(app);
+
+export default firestore;
+
+// THEME ADMIN PANEL SE CHANGE HOGA
