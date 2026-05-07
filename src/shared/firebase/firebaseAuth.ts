1c4cbdc013ba6f47c99088fdd82d93273c75c389
jembeekart-design <jembeekart@gmail.com>
2026-05-07 23:12:58 +0530

    Add firebaseAuth module for Firebase authentication

---
+++ src/shared/firebase/firebaseAuth.ts
@@ -0,0 +1,8 @@
+import { getAuth } from "firebase/auth";
+import firebaseApp from "./firebaseApp";
+
+const firebaseAuth = getAuth(firebaseApp);
+
+export default firebaseAuth;
+
+// THEME ADMIN PANEL SE CHANGE HOGA
