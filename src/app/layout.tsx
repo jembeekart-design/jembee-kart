32d14440df44b6ce0577698ed5c8e59209bbae91
jembeekart-design <jembeekart@gmail.com>
2026-05-16 18:02:46 +0530

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "JembeeKart",
+  description: "AI Powered Ecommerce Ecosystem",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
