44d067ec2f5f2d99ec64456c83090071c6693153
jembeekart-design <jembeekart@gmail.com>
2026-05-16 21:24:43 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,22 +4,25 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "JembeeKart",
-  description: "AI Ecommerce Ecosystem",
-  viewport: "width=device-width, initial-scale=1"
+  description: "AI Ecommerce Ecosystem"
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className="w-full overflow-x-hidden">
-        <div className="w-full overflow-x-hidden">
+
+      <body>
+
+        <div className="w-full overflow-hidden">
           {children}
         </div>
+
       </body>
+
     </html>
   );
 }
