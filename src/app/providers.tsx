e923c766ccc72b9918a2689275cef38a87eb4671
jembeekart-design <jembeekart@gmail.com>
2026-05-18 21:43:59 +0530

    Create providers.tsx

---
+++ src/app/providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import {
+  CartProvider
+} from "@/context/CartContext";
+
+export default function Providers({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <CartProvider>
+
+      {children}
+
+    </CartProvider>
+  );
+}
