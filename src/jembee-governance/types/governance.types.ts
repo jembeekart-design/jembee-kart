764d9ff806f72978e7162406b806e00e1ed0a155
Sadiya bashir <jembeekart@gmail.com>
2026-06-26 08:09:10 +0530

    Delete mlmGovernance from governance.types.ts
    Removed 'mlmGovernance' section from governance types.

--- src/jembee-governance/types/governance.types.ts
+++ src/jembee-governance/types/governance.types.ts
@@ -353,17 +353,6 @@ history: GovernanceHistory[];
   walletMismatchCount: number;
   profitLeakageCount: number;
 };
-history: GovernanceHistory[];
-
-mlmGovernance: {
-  healthScore: number;
-  totalOrdersAudited: number;
-  totalCommissionPaid: number;
-  totalCommissionReversed: number;
-  duplicateCommissionCount: number;
-  walletMismatchCount: number;
-  profitLeakageCount: number;
-};
 
 walletGovernance: {
   integrityScore: number;
