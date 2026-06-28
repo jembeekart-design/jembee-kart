5e9cff15ba103a5a3d0270da765fc11fad2efae7
Sadiya bashir <jembeekart@gmail.com>
2026-06-28 22:09:52 +0530

    Import Theme type from '@/types/theme'
    Refactor Theme type definition by importing from types.

--- src/components/admin/theme-builder/ColorPicker.tsx
+++ src/components/admin/theme-builder/ColorPicker.tsx
@@ -1,32 +1,6 @@
 "use client";
 
-type Theme = {
-  primaryColor: string;
-  secondaryColor: string;
-  backgroundColor: string;
-  cardColor: string;
-  textColor: string;
-  borderColor: string;
-  buttonRadius: number;
-  fontFamily: string;
-  borderRadius: string;
-
-cardRadius: number;
-inputRadius: number;
-
-headingSize: number;
-bodySize: number;
-fontWeight: string;
-
-shadow: string;
-shadowOpacity: number;
-borderWidth: number;
-
-buttonStyle: string;
-buttonSize: string;
-buttonHoverEffect: string;
-buttonIconPosition: string;
-};
+import type { Theme } from "@/types/theme";
 
 type Props = {
   theme: Theme;
