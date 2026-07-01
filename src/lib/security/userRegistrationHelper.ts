caed79acbe2313601fe4e739c9d6ef86e43a0986
Sadiya bashir <jembeekart@gmail.com>
2026-07-01 09:07:14 +0530

    Remove generateMarketingCode function
    Removed the generateMarketingCode function from userRegistrationHelper.ts.

--- src/lib/security/userRegistrationHelper.ts
+++ src/lib/security/userRegistrationHelper.ts
@@ -1,10 +1,3 @@
-export function generateMarketingCode(uid: string) {
-  const firstPart = uid.slice(0, 6).toUpperCase();
-  const lastPart = uid.slice(-4).toUpperCase();
-
-  return `JBK${firstPart}${lastPart}`;
-}
-
 export function buildParentChain(
   sponsorUid: string,
   sponsorParentChain: string[]
