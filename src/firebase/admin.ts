4392978e655c754e2aaf498555c41bf1d7b0462a
Sadiya bashir <jembeekart@gmail.com>
2026-07-21 20:06:15 +0530

    Update admin.ts

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,7 +1,6 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
-import { getStorage } from "firebase-admin/storage";
 
 const app =
   getApps().length > 0
@@ -20,6 +19,5 @@ const app =
 
 export const adminDb = getFirestore(app);
 export const adminAuth = getAuth(app);
-export const adminStorage = getStorage(app);
 
 export default app;
