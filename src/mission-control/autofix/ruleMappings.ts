98932ffc131b95f8bfbcfbf3f646d8ebaecf7f55
Sadiya bashir <jembeekart@gmail.com>
2026-07-23 06:45:23 +0530

    Update ruleMappings.ts

--- src/mission-control/autofix/ruleMappings.ts
+++ src/mission-control/autofix/ruleMappings.ts
@@ -1,14 +1,69 @@
-export const VERIFIED_RULES = [
+export interface RuleMapping {
+  name: string;
+  pattern: RegExp;
+  configPath: string;
+  safe: boolean;
+}
+
+export const VERIFIED_RULES: RuleMapping[] = [
+  {
+    name: "MLM Level 1",
+    pattern: /\blevel1\b/i,
+    configPath: "mlm.level1",
+    safe: true,
+  },
+  {
+    name: "MLM Level 2",
+    pattern: /\blevel2\b/i,
+    configPath: "mlm.level2",
+    safe: true,
+  },
+  {
+    name: "MLM Level 3",
+    pattern: /\blevel3\b/i,
+    configPath: "mlm.level3",
+    safe: true,
+  },
+  {
+    name: "Cashback Percent",
+    pattern: /\bcashback\b/i,
+    configPath: "cashback.cashbackPercent",
+    safe: true,
+  },
   {
     name: "Watch Reward",
-    pattern: "rewardAmount",
+    pattern: /\brewardAmount\b/i,
     configPath: "watchEarn.rewardAmount",
     safe: true,
   },
   {
     name: "Required Videos",
-    pattern: "requiredVideos",
+    pattern: /\brequiredVideos\b/i,
     configPath: "watchEarn.requiredVideos",
+    safe: true,
+  },
+  {
+    name: "Referral Bonus",
+    pattern: /\breferralBonus\b/i,
+    configPath: "referral.referralBonus",
+    safe: true,
+  },
+  {
+    name: "Joining Bonus",
+    pattern: /\bjoiningBonus\b/i,
+    configPath: "signup.joiningBonus",
+    safe: true,
+  },
+  {
+    name: "Shipping Charge",
+    pattern: /\bshippingCharge\b/i,
+    configPath: "shipping.shippingCharge",
+    safe: true,
+  },
+  {
+    name: "Minimum Transfer",
+    pattern: /\bminimumTransfer\b/i,
+    configPath: "mlmPage.minimumTransfer",
     safe: true,
   },
 ];
