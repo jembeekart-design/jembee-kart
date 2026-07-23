e191ef5e772106974c9da4bd7f3aa9642bd42ec3
Sadiya bashir <jembeekart@gmail.com>
2026-07-23 06:42:41 +0530

    Create ruleMappings.ts

---
+++ src/mission-control/autofix/ruleMappings.ts
@@ -0,0 +1,14 @@
+export const VERIFIED_RULES = [
+  {
+    name: "Watch Reward",
+    pattern: "rewardAmount",
+    configPath: "watchEarn.rewardAmount",
+    safe: true,
+  },
+  {
+    name: "Required Videos",
+    pattern: "requiredVideos",
+    configPath: "watchEarn.requiredVideos",
+    safe: true,
+  },
+];
