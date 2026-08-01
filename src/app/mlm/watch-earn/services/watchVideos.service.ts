0c11c271628cd0796bb67878de6b51462ddb55ca
Sadiya bashir <jembeekart@gmail.com>
2026-08-02 00:27:15 +0530

    Import WatchVideo type and export it

--- src/app/mlm/watch-earn/services/watchVideos.service.ts
+++ src/app/mlm/watch-earn/services/watchVideos.service.ts
@@ -1,4 +1,9 @@
-import { fetchWatchVideos } from "@/lib/mlm/watch-earn/fetchWatchVideos";
+import {
+  fetchWatchVideos,
+  type WatchVideo,
+} from "@/lib/mlm/watch-earn/fetchWatchVideos";
+
+export type { WatchVideo };
 
 export async function getWatchVideos() {
   return await fetchWatchVideos();
