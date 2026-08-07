078f6ef430245bdd778d787ff86e42f4e31843fc
jembeekart-design <jembeekart@gmail.com>
2026-08-07 14:32:06 +0530

    Remove ffmpeg tracing

--- next.config.ts
+++ next.config.ts
@@ -9,18 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-
-  serverExternalPackages: [
-    "ffmpeg-static",
-    "ffprobe-static",
-  ],
-
-  outputFileTracingIncludes: {
-    "/api/mlm/merge": [
-      "./node_modules/ffmpeg-static/**",
-      "./node_modules/ffprobe-static/**",
-    ],
-  },
 };
 
 export default nextConfig;
