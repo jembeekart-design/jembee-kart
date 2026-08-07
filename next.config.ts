1ebed84c2689ebadbf85e7f2b8e8d873722ad27e
jembeekart-design <jembeekart@gmail.com>
2026-08-07 13:35:51 +0530

    Fix ffmpeg on Vercel

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,22 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 
-        hostname:
-          "res.cloudinary.com"
-      }
-    ]
-  }
+  serverExternalPackages: [
+    "ffmpeg-static",
+    "ffprobe-static",
+  ],
+
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/ffmpeg-static/**",
+      "./node_modules/ffprobe-static/**",
+    ],
+  },
 };
 
 export default nextConfig;
