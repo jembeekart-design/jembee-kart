92ccd0429f482e69adb72bdb3aa3532e1e5e33fd
jembeekart <jembeekart@gmail.com>
2026-09-04 08:48:50 +0530

    Add Drive upload diagnostics

--- src/lib/mlm/watch-earn/uploadVideoToDrive.ts
+++ src/lib/mlm/watch-earn/uploadVideoToDrive.ts
@@ -16,7 +16,7 @@ export async function uploadVideoToDrive(
     fileType: file.type,
     chunkSize: CHUNK_SIZE,
     uploadUrlPresent: !!uploadUrl,
-  });\1console.log("[DRIVE_DEBUG] INITIAL_STATUS_BEGIN", {
+    console.log("[DRIVE_DEBUG] INITIAL_STATUS_BEGIN", {
     fileName: file.name,
     fileSize: file.size,
     fileType: file.type,
